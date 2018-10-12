54f314b95c03b93114d83e5ddc852c7c2029b010
Kirill Logachev <logachev.k@gmail.com>
2018-10-12 13:56:34 -0700

    Test

--- test.sh
+++ test.sh
@@ -2,8 +2,5 @@ echo "Sample script"
 echo "Login: $1"
 echo "Password: $2"
 echo "Non-secret: $3 $4"
-echo "$SECRET_CLIENT_ID"
-export MYNEWVARIABLE=$2
-export MYNEWVARIABLE2=$(echo $2 | base64)
-env
+echo "Password base64: $(echo $2 | base64)"
 echo "Done"
